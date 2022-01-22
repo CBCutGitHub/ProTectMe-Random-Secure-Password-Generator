396cec0b4f271b47cb8fdcfcbd0c5abc750f9628
MDemp <mdempsey.cbc.ut@gmail.com>
2022-01-21 19:24:10 -0600

    Created generate password using user input and shuffled arrays

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = getUserInput();
+  var password = generatePwd();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -19,6 +19,18 @@ var lowerCaseArray = [  'a',  'b',  'c',  'd',  'e',  "f",  'g',  'h',  'i',  'j
 var upperCaseArray = [  'A',  'B',  'C',  'D',  'E',  "F",  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N', 'O',  'P',  'Q',  'R',  'S',  'R',  'U',  'V',  'W',  'X',  'Y',  'Z'];
 
 // function to radomize array
+function shuffleArray(array) {
+
+  var getIndex = Math.floor(Math.random() * array.length); //returns random index NUMBER based on array length
+  var indexValue = array[getIndex]; //returns actual index VALUE based on random index number returned above
+
+  console.log ("randomIndex: "+ getIndex)
+  console.log("Actual index value: " + indexValue)
+  return indexValue; // makes indexValue available outside of function
+
+}
+//test shuffle array
+shuffleArray(upperCaseArray)
 
 // function to get and store user and put into object called user input
 function getUserInput(){
@@ -35,10 +47,37 @@ function getUserInput(){
     LowerCaseChoice: confirmLowercase,
     UpperCaseChoice: confirmUppercase
 
-
   }
    console.log(inputObject)
    return inputObject
 } 
 // generate password function that takes user input and radomize function and cerate a new randomize array
 // based on user input and dispaly array in html
+function generatePwd(){
+  var userInput = getUserInput() //userInput.pwdLength, userInput.NumericChoice
+  var finalPwd = []
+  var characterPool = []
+
+  if(userInput.NumericChoice===true){
+    characterPool = characterPool.concat(numericArray)
+characterPool.push(shuffleArray(numericArray))
+  }
+  if(userInput.SpecialChoice===true){
+    characterPool = characterPool.concat(specialArray)
+characterPool.push(shuffleArray(specialArray))
+  }
+  if(userInput.LowerCaseChoice===true){
+    characterPool = characterPool.concat(lowerCaseArray)
+characterPool.push(shuffleArray(lowerCaseArray))
+  }
+  if(userInput.UpperCaseChoice===true){
+    characterPool = characterPool.concat(upperCaseArray)
+characterPool.push(shuffleArray(upperCaseArray))
+  }
+for(var i = 0; i <userInput.pwdLength; i++){
+  var stageArray = shuffleArray(characterPool)
+  finalPwd.push(stageArray)
+}
+console.log(finalPwd)
+return finalPwd.join("")
+}
